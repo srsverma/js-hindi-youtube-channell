636257c63f5b0b963e5f0081a946044f873c73de
srsverma <skv7615@gmail.com>
2024-09-02 12:58:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,83 @@
+"use strict"  //treat all JS code as never version
+
+ //alert(3 + 3)  //we are using nodejs,not browser
+
+ console.log(3 
+    + 
+    3);  //code readability should be high  
+
+    let name ="suresh" 
+    let age = 23
+    let state = null 
+
+    // number => 2 to power 53
+    // bigint 
+    // string => ""
+    // boolean => true/false
+    //null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    // object 
+
+    console.log(typeof age);
+    console.log(typeof null);       //object
+    console.log(typeof undefined);   //undefined
+    console.log(typeof "age");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
