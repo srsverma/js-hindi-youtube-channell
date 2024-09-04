de16a903b55d880e72d38e4ae8f513911c35afd4
srsverma <skv7615@gmail.com>
2024-09-04 05:43:28 +0000

    summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,42 @@
+//Primitive datatype
+
+// 7 types :-  String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference(Non Primitive) datatype
+
+             //:- Array, Object, Functio
+             
+             
+            
+             const score = 100
+             const scoreValue = 100.88
+             
+             const isLoggedIn = false
+             const outsideTemp = null
+             let userEmail
+
+             const id = Symbol('123')
+             const anatherId = Symbol('123')
+
+             console.log(id === anatherId);
+
+
+             const bigNumber =  34567876544569876n
+             
+             const heros = ["shaktiman", "naagraj", "doga", "Array"];
+              
+             let myObj = {
+                name: "suresh"
+               // age: 24,
+             }
+
+             const myfunction = function(){
+                console.log("Hello world");
+                
+             }
+             
+
+             
+             console.log(typeof myfunction);
+             
