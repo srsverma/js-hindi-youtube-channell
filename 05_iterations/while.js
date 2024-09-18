035ab2d03dfbab39ef5546b832258204e090ecea
srsverma <skv7615@gmail.com>
2024-09-18 06:10:02 +0000

    do while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,25 @@
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+    
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+let arr =0
+while (arr < myArray.length) {
+    // console.log(`Hero is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// dowhile loop
+
+let score = 11       // pahle execute hota h fir 
+                        // condtion chek hoti h 
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
